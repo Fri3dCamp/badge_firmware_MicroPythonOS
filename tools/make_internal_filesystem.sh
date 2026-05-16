a317e510fc8bed90abd128db5ca7484796c232da
Thomas Farstrike <thomasverstreken@protonmail.com>
2026-05-16 22:10:05 +0200

    more apps

--- tools/make_internal_filesystem.sh
+++ tools/make_internal_filesystem.sh
@@ -1,10 +1,22 @@
 mydir=$(readlink -f "$0")
 mydir=$(dirname "$mydir")
 
-#"$HOME/ESP32_NES/internalsd_zips/roms/doom/=/roms/doom/" \
-#"$HOME/ESP32_NES/internalsd_zips/roms/gb/homebrew/=/roms/gb/homebrew" \
-#"$HOME/ESP32_NES/internalsd_zips/roms/gbc/homebrew/=/roms/gb/homebrew" \
-#"$HOME/ESP32_NES/internalsd_zips/roms/nes/homebrew/=/roms/nes/homebrew" \
+apps="com.micropythonos.duke_launcher com.micropythonos.doom_launcher com.micropythonos.nes_launcher com.micropythonos.gameboy_launcher"
+apps="$apps com.micropythonos.imageview com.micropythonos.confetti com.micropythonos.connect4 com.micropythonos.dj_addon com.micropythonos.musicplayer"
+apps="$apps com.quasikili.quasibird com.quasikili.quasinametag"
+
+appdir="$HOME/projects/MicroPythonOS/claude/MicroPythonOS/internal_filesystem/apps/"
+
+# Map app name to dest folder override if different from source name
+declare -A app_dest_overrides=(
+    [com.micropythonos.doom_launcher]=com.micropythonos.doom
+)
+
+app_args=()
+for app in $apps; do
+    dest="${app_dest_overrides[$app]:-$app}"
+    app_args+=("$appdir/$app=/apps/$dest/")
+done
 
 "$mydir"/mklittlefs_pack.sh -s 0x4F0000 -o "$mydir"/../littlefs2.bin \
 "$HOME/sources/DukeNano3D/outputs/E1L1-2_compromise.grp.zip=/roms/duke3d/Shareware_E1L1-2_compromise.grp.zip" \
@@ -12,6 +24,5 @@ mydir=$(dirname "$mydir")
 "$HOME/ESP32_NES/microsd_final/roms/gbc/best/=/roms/gb/best" \
 "$HOME/ESP32_NES/microsd_final/roms/nes/best/=/roms/nes/best" \
 "$HOME/ESP32_NES/internalsd_zips/roms/nes/homebrew/2048 (Blurred Lines).zip=/roms/nes/homebrew/2048 (Blurred Lines).zip" \
-"$HOME/ESP32_NES/internalsd_zips/roms/gbc/homebrew/Columns_DX.zip=/roms/gb/homebrew/Columns_DX.zip"
-
-
+"$HOME/ESP32_NES/internalsd_zips/roms/gbc/homebrew/Columns_DX.zip=/roms/gb/homebrew/Columns_DX.zip" \
+"${app_args[@]}"
