5aae7fc9d55949a7003fc191d97ac652e305f3e2
Thomas Farstrike <thomasverstreken@protonmail.com>
2026-05-22 08:37:09 +0200

    Split littlefs versions

--- tools/make_internal_filesystem.sh
+++ tools/make_internal_filesystem.sh
@@ -36,20 +36,31 @@ for app in $apps; do
     app_args+=("$appdir/$app=/apps/$app/")
 done
 
-#"$HOME/ESP32_NES/microsd_final/roms/gbc/best/=/roms/gb/best" \
-#"$HOME/sources/DukeNano3D/outputs/E1L1-2_nearcomplete.grp.zip=/roms/duke3d/Episode_1_Level_1_and_2_nearcomplete.grp.zip" \
 
-"$mydir"/mklittlefs_pack.sh -s 0x700000 -o "$mydir"/../littlefs2.bin \
-"internalsd_2026/=/" \
-"$HOME/ESP32_NES/microsd_final/roms/gb/best/=/roms/gb/best" \
-"$HOME/sources/DukeNano3D/outputs/E1L1-6_nearcomplete.grp.zip=/roms/duke3d/Episode_1_All_6_Levels_nearcomplete.grp.zip" \
-"$HOME/ESP32_NES/microsd_final/romart/gb/2/2C27EC70.png=/romart/gb/2/2C27EC70.png" \
-"$HOME/ESP32_NES/microsd_final/roms/gbc/best/Chessmaster, The (USA).zip=/roms/gb/best/Chessmaster, The (USA).zip" \
-"$HOME/ESP32_NES/microsd_final/romart/gbc/1/1C13DBB0.png=/romart/gb/1/1C13DBB0.png" \
-"$HOME/ESP32_NES/microsd_final/roms/nes/best/=/roms/nes/best" \
-"$HOME/ESP32_NES/microsd_final/romart/nes/0/053014FF.png=/romart/nes/0/053014FF.png" \
-"$HOME/ESP32_NES/microsd_final/roms/nes/homebrew/2048 (Blurred Lines).zip=/roms/nes/homebrew/2048 (Blurred Lines).zip" \
-"$HOME/ESP32_NES/microsd_final/romart/nes/2048 (Blurred Lines).png=/romart/nes/2048 (Blurred Lines).png" \
-"$HOME/ESP32_NES/microsd_final/roms/gbc/homebrew/Columns_DX.zip=/roms/gb/homebrew/Columns_DX.zip" \
-"$HOME/ESP32_NES/microsd_final/romart/gbc/Columns_DX.png=/romart/gb/Columns_DX.png" \
-"${app_args[@]}"
+make_for_year() {
+
+	year="$1"
+
+	#"$HOME/ESP32_NES/microsd_final/roms/gbc/best/=/roms/gb/best" \
+	#"$HOME/sources/DukeNano3D/outputs/E1L1-2_nearcomplete.grp.zip=/roms/duke3d/Episode_1_Level_1_and_2_nearcomplete.grp.zip" \
+
+	"$mydir"/mklittlefs_pack.sh -s 0x700000 -o "$mydir"/../littlefs2_$year.bin \
+	"internalsd_shared=/" \
+	"internalsd_$year=/" \
+	"$HOME/ESP32_NES/microsd_final/roms/gb/best/=/roms/gb/best" \
+	"$HOME/sources/DukeNano3D/outputs/E1L1-6_nearcomplete.grp.zip=/roms/duke3d/Episode_1_All_6_Levels_nearcomplete.grp.zip" \
+	"$HOME/ESP32_NES/microsd_final/romart/gb/2/2C27EC70.png=/romart/gb/2/2C27EC70.png" \
+	"$HOME/ESP32_NES/microsd_final/roms/gbc/best/Chessmaster, The (USA).zip=/roms/gb/best/Chessmaster, The (USA).zip" \
+	"$HOME/ESP32_NES/microsd_final/romart/gbc/1/1C13DBB0.png=/romart/gb/1/1C13DBB0.png" \
+	"$HOME/ESP32_NES/microsd_final/roms/nes/best/=/roms/nes/best" \
+	"$HOME/ESP32_NES/microsd_final/romart/nes/0/053014FF.png=/romart/nes/0/053014FF.png" \
+	"$HOME/ESP32_NES/microsd_final/roms/nes/homebrew/2048 (Blurred Lines).zip=/roms/nes/homebrew/2048 (Blurred Lines).zip" \
+	"$HOME/ESP32_NES/microsd_final/romart/nes/2048 (Blurred Lines).png=/romart/nes/2048 (Blurred Lines).png" \
+	"$HOME/ESP32_NES/microsd_final/roms/gbc/homebrew/Columns_DX.zip=/roms/gb/homebrew/Columns_DX.zip" \
+	"$HOME/ESP32_NES/microsd_final/romart/gbc/Columns_DX.png=/romart/gb/Columns_DX.png" \
+	"${app_args[@]}"
+
+}
+
+make_for_year 2024
+make_for_year 2026
